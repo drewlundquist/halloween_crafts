09e1bef31508132282f41011ecc80207ad245f47
SC\drew.lundquist <lundquistdrew@live.com>
2017-10-31 09:21:03 -0500

    changes made to all files in regards to the footer

---
+++ Halloween DIY/public_html/footer.php
@@ -0,0 +1,4 @@
+<footer> 
+<h5>Caitlin and Hannah's Halloween Crafts</h5> 
+<p> If you have any questions, contact Caitlin or Hannah by their school e-mail (which is currently not active yet).</p>
+</footer>
--- Halloween DIY/public_html/index.php
+++ Halloween DIY/public_html/index.php
@@ -50,9 +50,8 @@ include 'navigation.php';
 
 
 
-<footer> 
-<h5>Caitlin and Hannah's Halloween Crafts</h5> 
-<p> If you have any questions, contact Caitlin or Hannah by their school e-mail (which is currently not active yet).</p>
-</footer>
+<?php
+include 'footer.php';
+?>
 </body>
 </html>
