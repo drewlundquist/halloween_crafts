775f51a334e8dbf7330294c000bbe34cdb0e50b7
SC\drew.lundquist <lundquistdrew@live.com>
2017-10-31 08:57:51 -0500

    .php changes to index and creation of a navigation page

---
+++ Halloween DIY/public_html/navigation.php
@@ -0,0 +1,48 @@
+	<nav>
+	  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+
+            <!-- Three line "hamburger" icon for the collapsed navigation -->
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <!-- Title on nav bar -->
+          
+        </div>
+
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.html">Home</a>
+			</li>
+
+			<li>
+				<a href="about.html">About</a>
+			</li>
+
+			<li>
+				<a href="mummy.html">Cute Mummy Candles</a>
+			</li>
+
+			<li>
+				<a href="banner.html">Halloween Banner</a>
+			</li>
+
+			<li>
+				<a href="ghosts.html">Hanging Ghosts</a>
+			</li>
+
+			<li>
+				<a href="candy_corn.html">Candy Corn Decoration</a>
+
+			<li> <a href="signup.html">Sign Up</a>
+			<li> <a href="login.html">Login</a>
+			</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
