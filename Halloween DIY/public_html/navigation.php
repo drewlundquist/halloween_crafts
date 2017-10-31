51aeb29f644d1bf8d20ac9991b61f720026ebeb5
SC\drew.lundquist <lundquistdrew@live.com>
2017-10-31 09:01:14 -0500

    Movement of files for a test

--- Halloween DIY/public_html/navigation.php
+++ Halloween DIY/public_html/navigation.php
@@ -17,7 +17,7 @@
 
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="index.html">Home</a>
+            <li><a href="index.html">Home</a>
 			</li>
 
 			<li>
